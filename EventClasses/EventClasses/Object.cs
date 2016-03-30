e706a4ce6fd0bf61a90c8343b128926fca3f5660
Roy v.Oldenbeek <roy_v_oldenbeek@hotmail.com>
2016-03-30 12:26:00 +0200

    Changed some names

---
+++ EventClasses/EventClasses/Object.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventClasses
+{
+    class Object
+    {
+        public int ObjectID { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+
+        public Object(int id, string objname, string objdescription)
+        {
+            ObjectID = id;
+            Name = objname;
+            Description = objdescription;
+        }
+    }
+}
--- EventClasses/EventClasses/ObjectReservation.cs
+++ EventClasses/EventClasses/ObjectReservation.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EventClasses
 {
-    public class Reservation
+    public class ObjectReservation
     {
     }
 }
