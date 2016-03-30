7bdbdf672de7cde2d55a1f4e891e4ffb599e359b
Roy v.Oldenbeek <roy_v_oldenbeek@hotmail.com>
2016-03-30 11:55:58 +0200

    Added CheckIn and RFID Classes

---
+++ EventClasses/EventClasses/CheckIn.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventClasses
+{
+    class CheckIn
+    {
+        public RFID RfidTag { get; private set; }
+
+        public bool CheckedIn { get; private set; }
+
+        public void ChangeStatus()
+        {
+            if (this.CheckedIn == true)
+            {
+                this.CheckedIn = false;
+            }
+            else if(this.CheckedIn == false)
+            {
+                this.CheckedIn = true;
+            }
+        }
+    }
+}
--- EventClasses/EventClasses/RFID.cs
+++ EventClasses/EventClasses/RFID.cs
@@ -8,5 +8,18 @@ namespace EventClasses
 {
     public class RFID
     {
+        public User Usr { get; set; }
+
+        public int RfidTag { get; private set; }
+
+        public decimal Funds { get; private set; }
+
+
+        public void UpdateFunds(decimal update)
+        {
+            Funds += update;
+        }
+
+
     }
 }
