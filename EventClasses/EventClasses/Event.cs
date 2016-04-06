9d7fa17019f562fb6fdbef663bbb2fc3b78d8c7a
FireRageNL <wc.eendje@gmail.com>
2016-04-06 13:48:25 +0200

    Final push

--- EventClasses/EventClasses/Event.cs
+++ EventClasses/EventClasses/Event.cs
@@ -8,5 +8,37 @@ namespace EventClasses
 {
     class Event
     {
+        public Adress EventAddress { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public int EventID { get; private set; }
+
+
+        public Event(Adress adr, string nme, string desc, int id)
+        {
+            EventAddress = adr;
+            Name = nme;
+            Description = desc;
+            EventID = id;
+        }
+
+        public void UpdateEvent(Adress uadr = null, string uname = null, string udesc = null)
+        {
+            if (uadr != null)
+            {
+                EventAddress = uadr;
+            }
+            if (uname != null)
+            {
+                Name = uname;
+            }
+            if (udesc != null)
+            {
+                Description = udesc;
+            }
+        }
     }
 }
