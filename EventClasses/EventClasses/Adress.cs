5765628ac351916d8b9c0ebb441336cc44b6daec
FireRageNL <wc.eendje@gmail.com>
2016-04-06 09:59:03 +0200

    Finished adress and started with group and location

--- EventClasses/EventClasses/Adress.cs
+++ EventClasses/EventClasses/Adress.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,47 @@ namespace EventClasses
 {
     public class Adress
     {
+        public string Street { get; private set; }
+
+        public string Number { get; private set; }
+
+        public string City { get; private set; }
+
+        public string Country { get; private set; }
+
+        public string Zipcode { get; private set; }
+
+        public Adress(string str, string num, string cty, string cntry, string zip)
+        {
+            Street = str;
+            Number = num;
+            City = cty;
+            Country = cntry;
+            Zipcode = zip;
+            //push to db
+        }
+
+        public Adress FindAdress(string str)
+        {
+            //search on streetname
+            return null;
+        }
+
+        public void UpdateAdress(string str, string num, string cty, string cntry, string zip)
+        {
+            Street = str;
+            Number = num;
+            City = cty;
+            Country = cntry;
+            Zipcode = zip;
+            //push to db
+        }
+
+        public void DeleteAdress(Adress toDelete)
+        {
+            //delete from db
+        }
+
+
     }
 }
---
+++ EventClasses/EventClasses/Group.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventClasses
+{
+    class Group
+    {
+        public List<User> Users { get; private set; }
+        
+        public int GroupID { get; private set; }
+        
+        public string Groupname { get; private set; } 
+
+        public Location Loc { get; private set; }
+
+        public Group(string gname, int id)
+        {
+            GroupID = id;
+            Groupname = gname;
+        }
+
+        public void AddUserToGroup(User usr)
+        {
+            Users.Add(usr);
+        }
+    }
+}
