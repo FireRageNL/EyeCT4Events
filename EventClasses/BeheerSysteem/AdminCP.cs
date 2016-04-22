4dd96a3a9881541e1ad09a2ce65d1258bc340b33
FireRageNL <wc.eendje@gmail.com>
2016-04-22 12:48:28 +0200

    admincp fix

--- EventClasses/BeheerSysteem/AdminCP.cs
+++ EventClasses/BeheerSysteem/AdminCP.cs
@@ -24,6 +24,7 @@ namespace BeheerSysteem
                 btnGebruiker.Enabled = false;
                 btnEvent.Enabled = false;
                 btnGroep.Enabled = false;
+                btnReservering.Enabled = false;
             }
         }
 
